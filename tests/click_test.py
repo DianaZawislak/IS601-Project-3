ca3291f701faafad00243176a5062dfa188b8866
Diana Zawislak <diana.zawislak@icloud.com>
2022-04-17 19:24:25 -0400

    adjusted create log folder test

--- tests/click_test.py
+++ tests/click_test.py
@@ -11,16 +11,14 @@ def test_create_log_folder():
     response = runner.invoke(create_log_folder)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '../logs')
-    # make a directory if it doesn't exist
-    assert os.path.exists(logdir) == False
+    logdir = os.path.join(root, '../app/logs')
+    response = os.path.exists(logdir)
+    assert response == True
 
 def test_create_database():
     response = runner.invoke(create_database)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    dbdir = os.path.join(root, '../database')
-    # make a directory if it doesn't exist
-    assert os.path.exists(dbdir) == True
+    dbdir = os.path.join(root, '../app/database')
+    response = os.path.exists(dbdir)
+    assert  response == True
