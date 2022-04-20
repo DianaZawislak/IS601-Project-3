dc9776ad66498d110aeedf90c595b27d017b12ef
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-04-19 20:19:56 -0400

    added tests for all log files in new file called log_files_test

--- tests/click_test.py
+++ tests/click_test.py
@@ -15,10 +15,11 @@ def test_create_log_folder():
     response = os.path.exists(logdir)
     assert response == True
 
+
 def test_create_database():
     response = runner.invoke(create_database)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
     dbdir = os.path.join(root, '../database')
     response = os.path.exists(dbdir)
-    assert  response == True
+    assert response == True
