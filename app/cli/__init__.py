01825a88565c31db973fefc5a99615672c826bcf
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-04-27 17:25:00 -0400

    changed location of log folder and adjusted all log files tests accordingly as well as added tests for new 'useractivities'and 'song_upload' log files

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -11,7 +11,7 @@ def create_database():
     # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
-    dbdir = os.path.join(root, '../../database')
+    dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
     if not os.path.exists(dbdir):
         os.mkdir(dbdir)
