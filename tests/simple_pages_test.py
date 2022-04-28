eb9248721e2abfbb9cb04a38b48bded720cbe6b8
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-04-27 22:55:40 -0400

    adjusted simple_pages tests after eliminating welcome page

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,7 +5,7 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/about"' in response.data
-    assert b'href="/welcome"' in response.data
+    #assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
@@ -22,11 +22,11 @@ def test_request_about(client):
     assert response.status_code == 200
     assert b"About" in response.data
 
-def test_request_page1(client):
-    """This makes the index page"""
-    response = client.get("/welcome")
-    assert response.status_code == 200
-    assert b"welcome" in response.data
+#def test_request_page1(client):
+#   """This makes the index page"""
+#  response = client.get("/welcome")
+#   assert response.status_code == 200
+#    assert b"welcome" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
